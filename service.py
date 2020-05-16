06bd7857f9e017e8480f04c991213eaccdb38513
Bob <48949693+Bob984456043@users.noreply.github.com>
2020-05-16 23:38:03 +0800

    db.create_all()
    there is a lack of the db creating

--- service.py
+++ service.py
@@ -111,6 +111,8 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    
+db.create_all()
 # ==============================================================================================
 # Tool class include some usual methods
 # ==============================================================================================
